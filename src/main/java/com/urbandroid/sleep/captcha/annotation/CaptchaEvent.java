df833d440d247780fb88493f811f649ab023b126
Marcel Matula <marcel.matula@gmail.com>
2015-12-17 15:01:08 +0100

    javadoc added into generate release

--- src/main/java/com/urbandroid/sleep/captcha/annotation/CaptchaEvent.java
+++ src/main/java/com/urbandroid/sleep/captcha/annotation/CaptchaEvent.java
@@ -7,9 +7,6 @@ import com.urbandroid.sleep.captcha.CaptchaConstant;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 
-/**
- * Created by marcel on 11.12.15.
- */
 @StringDef({
         CaptchaConstant.CAPTCHA_BACK_INTENT_SOLVED,
         CaptchaConstant.CAPTCHA_BACK_INTENT_ALIVE
