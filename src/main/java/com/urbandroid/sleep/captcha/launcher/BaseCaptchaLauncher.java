2bf58d06ec6e01155986b13ebdd8cdbd7b01a3ac
Marcel Matula <marcel.matula@gmail.com>
2016-01-15 11:07:23 +0100

    Captcha Launger - starting singleTop via flags

--- src/main/java/com/urbandroid/sleep/captcha/launcher/BaseCaptchaLauncher.java
+++ src/main/java/com/urbandroid/sleep/captcha/launcher/BaseCaptchaLauncher.java
@@ -114,6 +114,8 @@ public class BaseCaptchaLauncher implements CaptchaLauncher {
         final Intent intent = prepareIntent(captchaInfo);
         Log.d(TAG, IntentUtil.traceIntent(intent));
 
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        intent.addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP);
         context.startActivity(intent);
     }
 
